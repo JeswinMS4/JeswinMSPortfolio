5c0ae061ecb340cc586af613349982660737189a
Jeswin MS <104427327+JeswinMS4@users.noreply.github.com>
2024-09-22 01:35:52 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
       primary: "#050816", // You can keep this dark background
       secondary: "#c3afa6", // Dark orange
-      tertiary: "#7A4000",  // A deeper dark orange for accents
+      tertiary: "#261501",  // A deeper dark orange for accents
       "black-100": "#40210F", // Darker brownish-black with a hint of orange
       "black-200": "#331A0D", // Even darker shade to blend with dark orange
       "white-100": "#f3f3f3", // You can keep this white or slightly warm it up
