27e268f3b95eefce3e5e65667f6a48a9be3a6871
Jeswin MS <104427327+JeswinMS4@users.noreply.github.com>
2024-09-22 01:24:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
-        tertiary: "#151030",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
+      primary: "#050816", // You can keep this dark background
+      secondary: "#FF8C00", // Dark orange
+      tertiary: "#7A4000",  // A deeper dark orange for accents
+      "black-100": "#40210F", // Darker brownish-black with a hint of orange
+      "black-200": "#331A0D", // Even darker shade to blend with dark orange
+      "white-100": "#f3f3f3", // You can keep this white or slightly warm it up
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
